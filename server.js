4085ccc59da699c39d28713c84c79340ac87f86c
shibaxhil-hub <shibaxhil@gmail.com>
2025-11-17 17:08:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,48 +5,25 @@ const io = require("socket.io")(http);
 
 const PORT = process.env.PORT || 10000;
 
-app.use(express.static(__dirname));
-
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/terms.html");
 });
 
-let waitingUser = null;
+app.use(express.static(__dirname));
 
 io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
-
-    if (!waitingUser) {
-        waitingUser = socket;
-        console.log("User waiting:", socket.id);
-        return;
-    }
-
-    const partner = waitingUser;
-    waitingUser = null;
-    console.log("Matched:", socket.id, "<->", partner.id);
-
-    socket.emit("matched");
-    partner.emit("matched");
+    console.log("User connected");
 
     socket.on("signal", (data) => {
-        partner.emit("signal", data);
-    });
-    partner.on("signal", (data) => {
-        socket.emit("signal", data);
+        socket.broadcast.emit("signal", data);
     });
 
     socket.on("disconnect", () => {
-        partner.emit("partnerDisconnected");
-        console.log("Disconnected:", socket.id);
-    });
-    partner.on("disconnect", () => {
-        socket.emit("partnerDisconnected");
-        console.log("Disconnected:", partner.id);
+        console.log("User disconnected");
+        socket.broadcast.emit("partnerDisconnected");
     });
 });
 
 http.listen(PORT, () => {
     console.log("Server running on port " + PORT);
 });
-
